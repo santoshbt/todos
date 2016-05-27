95d48839aa945438f026bb9e5515b5d511a74de5
santoshbt <bt.santosh01@gmail.com>
2016-05-27 18:00:47 +0530

    Add a todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,17 @@
 class TodosController < ApplicationController
 	def index
+		@todos = Todo.all
+	end
+
+	def new
+		@todo = Todo.new 
+	end
+
+	def create
+		puts params.inspect
+		puts params[:todo][:title].inspect
+		@todo = Todo.new(:title => params[:todo][:title])
+		@todo.save
+		redirect_to todos_path
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Todos::Application.routes.draw do
   root to: 'todos#index'
+  resources :todos, only: [:new, :create, :index]
 end
